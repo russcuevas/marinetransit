9a12fc6f33d864b3405944d60ddb08a85bc5fee8
Russel <russelcuevas0@gmail.com>
2025-02-06 18:29:05 +0800

    to-pass

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -27,91 +27,122 @@ if (isset($_POST['login'])) {
 }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login | MARINETRANSIT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MARINETRANSIT</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+
     <style>
-        body {
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-image: url('assets/admin/img/bg.jpg');
+        #test {
+            background-image: url('assets/admin/img/loginbg1.jpeg');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
+            height: 550px;
+            width: auto;
         }
 
-        .login-container {
-            max-width: 500px;
-            width: 100%;
-            padding: 40px;
-            background: white;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-            text-align: center;
+        body {
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 550px;
+            width: auto;
         }
 
-
-        .login-container h2 {
-            margin-bottom: 20px;
-            color: #002960;
+        input {
+            width: 300px !important;
+            background-color: #d9d9d9;
+            border: #000000 2px solid !important;
+            height: 50px !important;
         }
 
-        .form-control {
-            height: 45px;
-            border-radius: 5px;
+        .btn-facebook {
+            background-color: #182864 !important;
+            width: 300px !important;
+            height: 50px !important;
         }
 
-        .btn-login {
-            background: #0053a0;
-            color: white;
-            border: none;
-            height: 45px;
-            border-radius: 5px;
-            width: 100%;
-            transition: 0.3s;
-        }
-
-        .btn-login:hover {
-            background: #003b75;
-        }
-
-        .alert {
-            font-size: 14px;
+        h1 {
+            color: #002960 !important;
+            font-family: 'Recursive', 'Times New Roman', Times, serif;
         }
     </style>
 </head>
 
 <body>
-    <div class="login-container">
-        <img src="assets/admin/img/system_dashboard.jpg" alt="Admin Avatar" class="mb-3" width="80">
-        <h2>Admin Login</h2>
 
-        <?php if (isset($_SESSION['unsuccess'])): ?>
-            <div class="alert alert-danger" role="alert">
-                <?php echo $_SESSION['unsuccess'];
-                unset($_SESSION['unsuccess']); ?>
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div>
+                    <div class="card-body p-3 my-3">
+                        <!-- Nested Row within Card Body -->
+                        <form class="user" method="POST" action="">
+                            <div class="row justify-content-center" style="border: black 1px solid !important; background: white;">
+                                <div class="col-lg-8 d-flex align-items-center justify-content-center">
+                                    <div class="p-4">
+                                        <div class="row col-lg-12 d-flex align-items-center  justify-content-center">
+                                            <img class="img-profile mx-2 rounded-circle" src="assets/admin/img/avatar2.png" style="height: 50px; width: auto;">
+                                            <h2>Admin Login</h2>
+                                        </div>
+                                        <div class="form-group my-5">
+                                            <label for="user_name">Username</label>
+                                            <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Username">
+                                        </div>
+                                        <div class="form-group my-5">
+                                            <label for="user_password">Password</label>
+                                            <input type="password" class="form-control" name="user_password" id="user_password" placeholder="Password">
+                                        </div>
+                                        <div class="form-group my-5 d-flex justify-content-end">
+                                            <button type="submit" name="login" class="btn btn-info">LOGIN</button>
+                                        </div>
+                                        <?php if (isset($_SESSION['unsuccess'])): ?>
+                                            <div class="alert alert-danger" role="alert">
+                                                <?php echo $_SESSION['unsuccess'];
+                                                unset($_SESSION['unsuccess']); ?>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="row col-lg-12 ">
+                                        <div class="col-xl-12" id="test"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        <?php endif; ?>
-
-        <form method="POST" action="">
-            <div class="mb-3">
-                <input type="text" name="user_name" class="form-control" placeholder="Username" required>
-            </div>
-            <div class="mb-3">
-                <input type="password" name="user_password" class="form-control" placeholder="Password" required>
-            </div>
-            <button type="submit" class="btn btn-login" name="login">LOGIN</button>
-        </form>
+        </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap core JavaScript-->
+    <script src="assets/admin/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="assets/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="assets/admin/js/sb-admin-2.min.js"></script>
+
 </body>
 
 </html>
